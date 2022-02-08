f38b67d9a8738b67d764df607c97e6782787d73d
Caleb Maclennan <caleb@alerque.com>
2022-02-08 14:13:40 +0300

    feat(build): Generate mage page matching CLI args

--- build.rs
+++ build.rs
@@ -1,7 +1,8 @@
 use clap::IntoApp;
 use clap_complete::generator::generate_to;
 use clap_complete::shells::{Bash, Elvish, Fish, PowerShell, Zsh};
-use std::{collections, env, fs};
+use clap_mangen::Man;
+use std::{collections, env, fs, path::Path};
 use vergen::{vergen, Config};
 
 include!("src/cli.rs");
@@ -15,7 +16,27 @@ fn main() {
     };
     vergen(flags).expect("Unable to generate the cargo keys!");
     pass_on_configure_details();
+    generate_man_page();
     generate_shell_completions();
+}
+
+/// Generate shell completion files from CLI interface
+fn generate_man_page() {
+    let out_dir = match env::var_os("OUT_DIR") {
+        None => return,
+        Some(out_dir) => out_dir,
+    };
+    let man_dir = Path::new(&out_dir).join("manpage");
+    fs::create_dir_all(&man_dir).expect("Could not create directory in which to place man page");
+    let app = Cli::into_app();
+    let bin_name: &str = app
+        .get_bin_name()
+        .expect("Could not retrieve bin-name from generated Clap app");
+    let file = Path::new(&man_dir).join(format!("{}.1", bin_name));
+    let mut file = fs::File::create(&file).expect("Could not create man page file");
+    Man::new(app)
+        .render(&mut file)
+        .expect("Could not render man page");
 }
 
 /// Generate shell completion files from CLI interface
@@ -24,7 +45,7 @@ fn generate_shell_completions() {
         None => return,
         Some(out_dir) => out_dir,
     };
-    let completions_dir = path::Path::new(&out_dir).join("completions");
+    let completions_dir = Path::new(&out_dir).join("completions");
     fs::create_dir_all(&completions_dir)
         .expect("Could not create directory in which to place completions");
     let app = Cli::into_app();
